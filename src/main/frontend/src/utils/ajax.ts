6b50a9de8e6ad172c04c357f46773dbf36e74763
Heejeong Kim <developer.heejeong@gmail.com>
2024-08-04 01:07:18 +0900

    [2024.08.04] 파일 업로드 구현

--- src/main/frontend/src/utils/ajax.ts
+++ src/main/frontend/src/utils/ajax.ts
@@ -7,36 +7,26 @@ interface AjaxProps {
     body?: Object | {}
     token?: TokenState | undefined
     csrf?: CsrfTokenState | undefined
+    file?: File | undefined
 }
 const Ajax = async (props: AjaxProps): Promise<any> => {
-    const initParams = props.method === 'GET' ? {
+    const initParams = {
         method: props.method,
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': ''
+            'Authorization': `${props.token?.bearerType} ${props.token?.accessToken}`
         }
-    }: {
-        method: props.method,
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': ''
-        },
-        body: JSON.stringify(props.body)
-    };
+    }
 
     if(props.csrf) {
         // @ts-ignore
         initParams.headers[props.csrf.headerName] = props.csrf.token;
     }
-    if(props.token) {
-        initParams.headers.Authorization = `${props.token.bearerType} ${props.token.accessToken}`;
-    }
 
     if(props.body && Object.keys(props.body).length > 0) {
+        // @ts-ignore
         initParams.body = JSON.stringify(props.body)
     }
-
-    console.debug('param', initParams)
 
     return new Promise((resolve, reject) => {
         fetch(props.url, initParams)
@@ -63,4 +53,47 @@ const Ajax = async (props: AjaxProps): Promise<any> => {
     });
 };
 
-export {Ajax};
+const AjaxFile = async (props: AjaxProps): Promise<any> => {
+    const initParams = {
+        method: props.method,
+        headers: {
+            'Authorization': `${props.token?.bearerType} ${props.token?.accessToken}`
+        }
+    };
+
+    if(props.csrf) {
+        // @ts-ignore
+        initParams.headers[props.csrf.headerName] = props.csrf.token;
+    }
+
+    const formData = new FormData();
+    props.file && formData.append('file', props.file);
+    // @ts-ignore
+    initParams.body = formData
+
+    return new Promise((resolve, reject) => {
+        fetch(props.url, initParams)
+            .then(response => {
+                if(response.ok) {
+                    return response;
+                }
+                if (response.status === 401) {
+                    throw new Error('로그인이 필요합니다');
+                }
+                if(response.status === 504) {
+                    throw new Error('API 연결에 실패했습니다.<br>관리자에게 문의바랍니다.')
+                }
+                return response.text().then(message => {
+                    throw new Error(message);
+                });
+            })
+            .then(response=> {
+                resolve(response)
+            })
+            .catch(reason => {
+                reject(reason)
+            });
+    });
+};
+
+export {Ajax, AjaxFile};
